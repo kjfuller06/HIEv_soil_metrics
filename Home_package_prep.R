be60439df5bd271d53357cbc7579957651e9ad33
Kate Fuller <kjfuller06@gmail.com>
2019-07-01 13:20:35 +1000

    Need a file for preparing my home laptop

---
+++ Home_package_prep.R
@@ -0,0 +1,12 @@
+#load packages for HIEv data
+.libPaths("C:/Users/kjf00/Documents/R/win-library/3.6")
+
+#load packages for downloadTOA5 function
+library(tidyr)
+library(gdata)
+library(devtools)
+install_bitbucket("remkoduursma/HIEv")
+library(HIEv)
+setToken("vyyk6yyDYMPwaymASW7Q")
+library(reshape2)
+library(doBy)
