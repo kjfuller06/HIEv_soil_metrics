4dd92e12d5cf211e5c37e0f45ea3692f6d9ee300
Kate Fuller <kjfuller06@gmail.com>
2019-09-29 10:32:44 +1000

    Calling more from the csv so it's more easily edited outside of R

--- RewriteLabels.R
+++ RewriteLabels.R
@@ -1,22 +1,20 @@
 sp="Fes"
-d<-"October 2019"
 
 labels<-read.csv("labels.csv")
+df1<-labels[labels$PlotType==sp,]
 
 start<-function(){
   plot(1,bty='n',xaxt='n',yaxt='n',type="n",xlab="",ylab="",xlim=c(0, 10),ylim=c(0, 10))
 }
 blanklabel<-function(){
   start()
-  text(5,9,labels="PACE-Power",cex=1.5)
-  text(5,8,labels="Aboveground Harvest",cex=1.5)
-  text(5,7,labels=d,cex=1.5)
+  text(5,9,labels=df1$Lab[1],cex=1.5)
+  text(5,8,labels=df1$Harvest[1],cex=1.5)
+  text(5,7,labels=df1$Date[1],cex=1.5)
   text(5,6,labels=sp,cex=1.5)
   text(0,3,labels="Subplot ID__________",pos=4)
   text(0,2,labels="Sample Type___________",pos=4)
 }
-
-df1<-labels[labels$PlotType==sp,]
 
 #blank- no subplot ID
 pdf(paste(sp,"blank.pdf"))
@@ -34,9 +32,9 @@ par(mfrow=c(4,2))
 if(df1$NoSp[1]==1){
   for (i in df1$SubplotID){
     start()
-    text(5,9,labels="PACE-Power",cex=1.5)
-    text(5,8,labels="Aboveground Harvest",cex=1.5)
-    text(5,7,labels=d,cex=1.5)
+    text(5,9,labels=df1$Lab[1],cex=1.5)
+    text(5,8,labels=df1$Harvest[1],cex=1.5)
+    text(5,7,labels=df1$Date[1],cex=1.5)
     text(5,6,labels=sp,cex=1.5)
     text(0,3,labels=i,pos=4)
     text(0,2,labels="Live target",pos=4)
@@ -44,18 +42,18 @@ if(df1$NoSp[1]==1){
   } else{
     for (i in df1$SubplotID){
       start()
-      text(5,9,labels="PACE-Power",cex=1.5)
-      text(5,8,labels="Aboveground Harvest",cex=1.5)
-      text(5,7,labels=d,cex=1.5)
+      text(5,9,labels=df1$Lab[1],cex=1.5)
+      text(5,8,labels=df1$Harvest[1],cex=1.5)
+      text(5,7,labels=df1$Date[1],cex=1.5)
       text(5,6,labels=sp,cex=1.5)
       text(0,3,labels=i,pos=4)
       text(0,2,labels=paste("Live",df1$Sp1[1]),pos=4)
     }
     for (i in df1$SubplotID){
       start()
-      text(5,9,labels="PACE-Power",cex=1.5)
-      text(5,8,labels="Aboveground Harvest",cex=1.5)
-      text(5,7,labels=d,cex=1.5)
+      text(5,9,labels=df1$Lab[1],cex=1.5)
+      text(5,8,labels=df1$Harvest[1],cex=1.5)
+      text(5,7,labels=df1$Date[1],cex=1.5)
       text(5,6,labels=sp,cex=1.5)
       text(0,3,labels=i,pos=4)
       text(0,2,labels=paste("Live",df1$Sp2[1]),pos=4)
@@ -75,9 +73,9 @@ par(mar=c(1,0,1,0))
 par(mfrow=c(4,2))
 for (i in df1$SubplotID){
   start()
-  text(5,9,labels="PACE-Power",cex=1.5)
-  text(5,8,labels="Aboveground Harvest",cex=1.5)
-  text(5,7,labels=d,cex=1.5)
+  text(5,9,labels=df1$Lab[1],cex=1.5)
+  text(5,8,labels=df1$Harvest[1],cex=1.5)
+  text(5,7,labels=df1$Date[1],cex=1.5)
   text(5,6,labels=sp,cex=1.5)
   text(0,3,labels=i,pos=4)
   text(0,2,labels="Dead",pos=4)
@@ -97,9 +95,9 @@ par(mfrow=c(4,2))
 if(df1$NoSp[1]==1){
   for (i in df1$SubplotID){
     start()
-    text(5,9,labels="PACE-Power",cex=1.5)
-    text(5,8,labels="Aboveground Harvest",cex=1.5)
-    text(5,7,labels=d,cex=1.5)
+    text(5,9,labels=df1$Lab[1],cex=1.5)
+    text(5,8,labels=df1$Harvest[1],cex=1.5)
+    text(5,7,labels=df1$Date[1],cex=1.5)
     text(5,6,labels=sp,cex=1.5)
     text(0,3,labels=i,pos=4)
     text(0,2,labels="Catunda",pos=4)
@@ -107,18 +105,18 @@ if(df1$NoSp[1]==1){
 } else{
   for (i in df1$SubplotID){
     start()
-    text(5,9,labels="PACE-Power",cex=1.5)
-    text(5,8,labels="Aboveground Harvest",cex=1.5)
-    text(5,7,labels=d,cex=1.5)
+    text(5,9,labels=df1$Lab[1],cex=1.5)
+    text(5,8,labels=df1$Harvest[1],cex=1.5)
+    text(5,7,labels=df1$Date[1],cex=1.5)
     text(5,6,labels=sp,cex=1.5)
     text(0,3,labels=i,pos=4)
     text(0,2,labels=paste("Catunda",df1$Sp1[1]),pos=4)
   }
   for (i in df1$SubplotID){
     start()
-    text(5,9,labels="PACE-Power",cex=1.5)
-    text(5,8,labels="Aboveground Harvest",cex=1.5)
-    text(5,7,labels=d,cex=1.5)
+    text(5,9,labels=df1$Lab[1],cex=1.5)
+    text(5,8,labels=df1$Harvest[1],cex=1.5)
+    text(5,7,labels=df1$Date[1],cex=1.5)
     text(5,6,labels=sp,cex=1.5)
     text(0,3,labels=i,pos=4)
     text(0,2,labels=paste("Catunda",df1$Sp2[1]),pos=4)
