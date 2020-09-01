568d3bd2ab6c52c280a4342789a7386bce4fd151
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-08-31 20:03:57 -0500

    Fixed some run errors

--- Churchilletal2020_Figure2_raw_surface_temperature_plots.R
+++ Churchilletal2020_Figure2_raw_surface_temperature_plots.R
@@ -1,10 +1,10 @@
-#visualise raw surface temperature data####
-#format for graphing
+# #visualise raw surface temperature data####
+# #format for graphing
 max<-subset(surf,select=c(-value,-minT))
-max<-split(max,droplevels(max$Treatment))
+max<-split(max,droplevels(as.factor(max$Treatment)))
 
 min<-subset(surf,select=c(-value,-maxT))
-min<-split(min,droplevels(min$Treatment))
+min<-split(min,droplevels(as.factor(min$Treatment)))
 
 
 
@@ -17,7 +17,7 @@ ymin<-min(airT$value,na.rm=T)
 tiff(file = paste("FIELD_Max_Surface_Temp_",sD,"_",eD,".tiff",sep=""), width = 1600, height = 1050, units = "px", res = 200)
 #Raw surface temperature plot
 par(mar=c(5.1,4.1,4.1,3.1))
-with(max[[1]],plot(maxT[,1] ~ Date, 
+with(max[[1]],plot(maxT ~ Date, 
      type = "l",
      bty='l',
      ylim = c(ymin-0.05,ymax),
@@ -30,11 +30,11 @@ abline(h=45,lty=2)
 # with(max[[1]],rect(min(Date)-25,45,max(Date)+25,65,col=adjustcolor(1,alpha.f=0.15),border=NA))
 mtext(side=2,expression(paste("Temperature (",degree~C,")")),padj=-2.5)
 # with(max[[3]],polygon(c(Date,rev(Date)),c(maxT[,3],rev(maxT[,2])),col=adjustcolor("red",alpha.f=0.65),border=NA))
-with(max[[3]],points(maxT[,1] ~ Date, type = "l",col="red"))
+with(max[[3]],points(maxT ~ Date, type = "l",col="red"))
 # with(max[[4]],polygon(c(Date,rev(Date)),c(lower,rev(upper)),col=adjustcolor("red4",alpha.f=0.65),border=NA))
-with(max[[4]],points(maxT[,1] ~ Date, type = "l",col="red4",lty=2))
+with(max[[4]],points(maxT ~ Date, type = "l",col="red4",lty=2))
 # with(max[[2]],polygon(c(Date,rev(Date)),c(lower,rev(upper)),col=adjustcolor("lightskyblue",alpha.f=0.85),border=NA))
-with(max[[2]],points(maxT[,1] ~ Date, type = "l",col="lightskyblue",lty=2))
+with(max[[2]],points(maxT ~ Date, type = "l",col="lightskyblue",lty=2))
 with(airT,points(value~Date,type="l"))
 # legend("topleft", y = NULL,
 #        legend=c("aT-Con","aT-Drt","eT-Con","eT-Drt"), 
@@ -56,7 +56,7 @@ ymin<-min(surf$minT,na.rm=T)
 tiff(file = paste("FIELD_Min_Surface_Temp_",sD,"_",eD,".tiff",sep=""), width = 1600, height = 1050, units = "px", res = 200)
 #Raw surface temperature plot
 par(mar=c(5.1,4.1,4.1,3.1))
-with(min[[1]],plot(minT[,1] ~ Date, 
+with(min[[1]],plot(minT ~ Date, 
                    type = "l",
                    bty='l',
                    ylim = c(ymin-0.05,ymax),
@@ -69,11 +69,11 @@ abline(h=-5,lty=2)
 # with(min[[1]],rect(min(Date)-25,-5,min(Date)+25,-25,col=adjustcolor(1,alpha.f=0.15),border=NA))
 mtext(side=2,expression(paste("Surface Temperature (",degree~C,")")),padj=-2.5)
 # with(min[[3]],polygon(c(Date,rev(Date)),c(minT[,3],rev(minT[,2])),col=adjustcolor("red",alpha.f=0.65),border=NA))
-with(min[[3]],points(minT[,1] ~ Date, type = "l",col="red"))
+with(min[[3]],points(minT ~ Date, type = "l",col="red"))
 # with(min[[4]],polygon(c(Date,rev(Date)),c(lower,rev(upper)),col=adjustcolor("red4",alpha.f=0.65),border=NA))
-with(min[[4]],points(minT[,1] ~ Date, type = "l",col="red4",lty=2))
+with(min[[4]],points(minT ~ Date, type = "l",col="red4",lty=2))
 # with(min[[2]],polygon(c(Date,rev(Date)),c(lower,rev(upper)),col=adjustcolor("lightskyblue",alpha.f=0.85),border=NA))
-with(min[[2]],points(minT[,1] ~ Date, type = "l",col="lightskyblue",lty=2))
+with(min[[2]],points(minT ~ Date, type = "l",col="lightskyblue",lty=2))
 with(airT,points(value~Date,type="l"))
 # legend("topleft", y = NULL,
 #        legend=c("aT-Con","aT-Drt","eT-Con","eT-Drt"), 
