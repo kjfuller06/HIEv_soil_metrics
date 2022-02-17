343de76b1ea0edcc0ac2975e6094238e92b3435d
kjfuller06 <kjfuller06@gmail.com>
2022-02-17 11:40:01 -0600

    Cleaning house

--- M14_package_prep.R
+++
@@ -1,9 +0,0 @@
-#load packages for downloadTOA5 function
-# library(gdata)
-library(devtools)
-library(data.table)
-install_bitbucket("remkoduursma/HIEv")
-library(HIEv)
-setToken("vyyk6yyDYMPwaymASW7Q")
-library(reshape2)
-library(tidyverse)
